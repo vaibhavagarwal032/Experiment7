1a9c034ea43a82f68cc001b64e2ebc39b9e3a8dd
vaibhav <vaibhavagarwal032@gmail.com>
2018-11-03 01:39:05 +0530

    Initial commit

---
+++ app/src/main/java/com/vaibhav/experiment7/Second.java
@@ -0,0 +1,25 @@
+package com.vaibhav.experiment7;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Second extends AppCompatActivity {
+TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        txt=findViewById(R.id.text);
+        Intent intent = getIntent();
+
+        String fName = intent.getStringExtra("firstName");
+        String lName = intent.getStringExtra("lastName");
+        String email=intent.getStringExtra("email");
+        String contact=intent.getStringExtra("contact");
+
+        txt.setText("First Name: "+fName+"\nLast Name: "+lName+"\nEmail: "+email+"\nContact: "+contact);
+    }
+}
